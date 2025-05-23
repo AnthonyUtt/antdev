6a44f71e2f7fbb0fdecbc80f7a335840729412cb
AnthonyUtt <anthony@anthonyutt.dev>
2025-05-23 17:04:40 -0400

    feat: new sass setup + CUBE css

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,56 @@
-export default async function(config) {
-  config.addLayoutAlias("default", "layouts/base.liquid");
+import path from 'path';
+import * as sass from 'sass';
 
-  return {
-    dir: {
-      input: "src",
-      output: "public",
-    },
-    templateFormats: ["html", "liquid", "11ty.js"],
-  };
+const BASE_CONFIG = {
+  dir: {
+    input: "src",
+    output: "public",
+  },
+  templateFormats: ["html", "liquid", "11ty.js"],
 };
+
+export default async function(config) {
+  setupLayouts(config);
+  setupAssetPassthrough(config);
+  setupSassPipeline(config);
+
+  return BASE_CONFIG;
+};
+
+function setupLayouts(config) {
+  config.addLayoutAlias("default", "layouts/default.liquid");
+  config.addLayoutAlias("article", "layouts/article.liquid");
+}
+
+function setupAssetPassthrough(config) {
+  config.addPassthroughCopy("src/assets");
+}
+
+function setupSassPipeline(config) {
+  config.addExtension("scss", {
+    outputFileExtension: "css",
+    useLayouts: false,
+    compile: async function(inputContent, inputPath) {
+      const parsed = path.parse(inputPath);
+      // Don't compile finenames that start with an underscore
+      if (parsed.name.startsWith("_")) {
+        return;
+      }
+
+      let result = sass.compileString(inputContent, {
+        loadPaths: [
+          parsed.dir || ".",
+          this.config.dir.includes,
+        ],
+        sourceMap: false,
+      });
+
+      this.addDependencies(inputPath, result.loadedUrls);
+
+      return async () => {
+        return result.css;
+      };
+    },
+  });
+  config.addTemplateFormats("scss");
+}
